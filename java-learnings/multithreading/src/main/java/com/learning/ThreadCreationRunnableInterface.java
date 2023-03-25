4053fb79980a86c4bd1f23e021906476bd5afe54
goeld <goel_deepakin@yahoo.com>
2023-03-25 16:49:40 +0800

    Theads using runnable interface : catch exception

--- java-learnings/multithreading/src/main/java/com/learning/ThreadCreationRunnableInterface.java
+++ java-learnings/multithreading/src/main/java/com/learning/ThreadCreationRunnableInterface.java
@@ -1,4 +1,28 @@
 package com.learning;
 
 public class ThreadCreationRunnableInterface {
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(() -> {
+            System.out.println("Current Thread name : " + Thread.currentThread().getName());
+            System.out.println("Inside Current thread prioroty : " + Thread.currentThread().getPriority());
+            throw new RuntimeException("Uncaught Exception");
+        });
+
+        thread.setName("My first thread");
+        thread.setPriority(Thread.MAX_PRIORITY);
+        thread.setUncaughtExceptionHandler( new Thread.UncaughtExceptionHandler() {
+
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                System.out.println("A critical error has happened");
+                e.printStackTrace();
+            }
+        });
+
+        System.out.println("Current thread name is " + Thread.currentThread().getName());
+        thread.start();
+//        Thread.sleep(1000);
+        System.out.println("Current thread name is " + Thread.currentThread().getName());
+    }
 }
